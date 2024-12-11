ecde71f8767c9fa17198a2fdecd1073aebf3fadf
bk <naturalbb360@gmail.com>
2024-12-12 06:03:40 +0900

    feat(Coach, Coaches): 코치들의 정보를 담는 클래스 추가

---
+++ src/main/java/menu/Coach.java
@@ -0,0 +1,14 @@
+package menu;
+
+import java.util.List;
+
+public class Coach {
+    String name;
+    List<String> cantEatFood;
+
+    public Coach(String name, List<String> cantEatFood) {
+        this.name = name;
+        this.cantEatFood = cantEatFood;
+    }
+
+}
---
+++ src/main/java/menu/Coaches.java
@@ -0,0 +1,16 @@
+package menu;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Coaches {
+    List<Coach> coaches;
+
+    public Coaches() {
+        this.coaches = new LinkedList<>();
+    }
+
+    public void add(String name, List<String> cantEatFoods){
+        coaches.add(new Coach(name, cantEatFoods));
+    }
+}
